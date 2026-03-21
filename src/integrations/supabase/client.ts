055813c778920f3f0bbfac4f051579fe06e21976
shri subashini m <shrisubashini2007@gmail.com>
2026-03-21 18:06:28 +0530

    Update Supabase client to use ANON key

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -8,7 +8,7 @@ const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: localStorage,
     persistSession: true,
