f645c464565b511461791746eedba3638f566cdc
shri subashini m <shrisubashini2007@gmail.com>
2026-03-27 16:06:55 +0530

    Update index.ts

--- supabase/functions/ai-summarize/index.ts
+++ supabase/functions/ai-summarize/index.ts
@@ -4,16 +4,15 @@ import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers":
-    "authorization, x-client-info, apikey, content-type, x-supabase-client-platform, x-supabase-client-platform-version, x-supabase-client-runtime, x-supabase-client-runtime-version",
-  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "authorization, x-client-info, apikey, content-type",
+   "Access-Control-Allow-Methods": "POST, OPTIONS",
 };
 
 serve(async (req) => {
   // 🔥 HANDLE PREFLIGHT (VERY IMPORTANT)
   if (req.method === "OPTIONS") {
-    return new Response(null, { headers: corsHeaders });
-  }
-
+  return new Response(null, { headers: corsHeaders });
+}
   try {
     const { text } = await req.json();
 
